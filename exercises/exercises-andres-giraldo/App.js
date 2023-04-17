bffc8e0569cb58a88d0866bb12281119a312e9a2
Andres Giraldo <andresganc@gmail.com>
2023-04-17 12:02:35 -0500

    New: Exercises Andres Giraldo

---
+++ exercises/exercises-andres-giraldo/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+// Components
+import Card01 from './src/cards/Card01';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Card01 />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10
+  },
+});
