26582dd438f1d172a0707aff159594986c31b78a
Andres Giraldo <andresganc@gmail.com>
2023-07-10 12:17:20 -0500

    New: Navigation Menu

--- exercises/exercises-andres-giraldo/App.js
+++ exercises/exercises-andres-giraldo/App.js
@@ -1,15 +1,28 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+const Menu = createDrawerNavigator()
 
 // Components
 import Card01 from './src/cards/Card01';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Card01 />
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Card01 />
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <Menu.Navigator>
+        <Menu.Screen name='Home' component={Home} />
+        <Menu.Screen name='AboutUs' component={AboutUs} />
+        <Menu.Screen name='ContactUs' component={ContactUs} />
+      </Menu.Navigator>
+    </NavigationContainer>
   );
 }
 
